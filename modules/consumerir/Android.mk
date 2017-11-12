c279074a250def519871e93785396ac51e78a60e
viscropst <viscropme@gmail.com>
2017-11-12 12:22:13 +0800

    Revert the power and consumerir changes

--- modules/consumerir/Android.mk
+++ modules/consumerir/Android.mk
@@ -1,5 +1,4 @@
-#
-# Copyright 2017 The Android Open Source Project
+# Copyright (C) 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,28 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
-# This contains the module build definitions for the hardware-specific
-# components for this device.
-#
-# As much as possible, those components should be built unconditionally,
-# with device-specific names to avoid collisions, to avoid device-specific
-# bitrot and build breakages. Building a component unconditionally does
-# *not* include it on all devices, so it is safe even with hardware-specific
-# components.
 
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
-endif
-
+LOCAL_MODULE := consumerir.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := power.c
+LOCAL_SRC_FILES := consumerir.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_MODULE := power.msm8992
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
